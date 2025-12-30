195ad6c15e05f375bd016720987bc0bd413c5202
Adam Halka <galkaadam4@gmail.com>
2025-12-30 15:48:47 +0100

    Solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-slim
+LABEL maintainer="galkaadam4@gmail.com"
+
+ENV PYTHONBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+
+    if not api_key:
+        print("API key not set")
+        return
+
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": api_key,
+        "q": "Paris",
+        "aqi": "no"
+    }
+
+    response = requests.get(url, params)
+    data = response.json()
+
+    city = data["location"]["name"]
+    temp = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"City: {city}, Temperature: {temp}, Condition: {condition}")
 
 
 if __name__ == "__main__":
